110892e55c8eff14f040c7516099919bca7cce25
Oskar Johansson <oskar@irisnet.se>
2019-07-09 20:37:32 +0200

    Workning firmware
    The firmware is now working with BT-device and Car bluetooth app on android

--- Software/Software/src/main.cpp
+++ Software/Software/src/main.cpp
@@ -11,9 +11,60 @@ SoftwareSerial BTserial(BT_RX, BT_TX);
 #define rWheelPin 11
 Servo lWheel, rWheel;
 
- 
+#define NOSPEED 90
+#define DEFAULTSPEED 20
+#define TURNSPEED 20
+long lasttime = millis(); 
+
 void doSomething(char cmd) {
-  Serial.println(cmd);
+  lasttime = millis(); 
+  int rSpeed = NOSPEED, lSpeed = NOSPEED;
+  switch (cmd)
+  {
+  // FORWARD   
+
+  case 'P':
+    lSpeed = 0;
+    rSpeed = 0;
+    break;
+  case 'O':
+    lSpeed = 180;
+    rSpeed = 180;
+    break;
+  case 'R': // Right
+    rSpeed += DEFAULTSPEED;
+    break;
+  case 'L':
+    lSpeed += DEFAULTSPEED;
+    break;
+  case 'F':
+    rSpeed += DEFAULTSPEED; lSpeed += DEFAULTSPEED;
+    break;
+  case 'I':
+    rSpeed += DEFAULTSPEED + TURNSPEED; lSpeed += DEFAULTSPEED;  
+    break;
+  case 'G':
+    rSpeed += DEFAULTSPEED; lSpeed += DEFAULTSPEED + TURNSPEED;
+    break;
+
+  // REVERSE
+  case 'B':
+    rSpeed -= DEFAULTSPEED; lSpeed -= DEFAULTSPEED; 
+    break;   
+  case 'H':
+    rSpeed -= DEFAULTSPEED; lSpeed -= (DEFAULTSPEED + TURNSPEED);  
+    break;
+  case 'J':
+    rSpeed -= (DEFAULTSPEED + TURNSPEED); lSpeed -= DEFAULTSPEED;
+    break;
+
+
+  default:
+    break;
+  }
+  lWheel.write(lSpeed);
+  rWheel.write(rSpeed);
+  Serial.print("Command: ");Serial.print(cmd); Serial.print(" left: "); Serial.print(lSpeed); Serial.print(" right: "); Serial.println(rSpeed);
 }
 
 void setup() {
@@ -25,9 +76,21 @@ void setup() {
 }
 
 void loop() {
-  if (BTSerial.available()) // read from HC-05 and send to Arduino Serial Monitor
-    doSomething(BTSerial.read()));
+  if (Serial.available()) // read from HC-05 and send to Arduino Serial Monitor
+    doSomething(Serial.read());
+  if (BTserial.available()) // read from HC-05 and send to Arduino Serial Monitor
+    doSomething(BTserial.read());
+
+
+  if((millis() - lasttime) > 500) {
+    lWheel.write(NOSPEED);
+    rWheel.write(NOSPEED);
+  }
+
+  /* if (BTserial.available()) // read from HC-05 and send to Arduino Serial Monitor
+    doSomething(BTserial.read());
 
 if (Serial.available()) // Keep reading from Arduino Serial Monitor and send to HC-05
-  BTSerial.write(Serial.read());
+  BTserial.write(Serial.read());
+  */
 }
